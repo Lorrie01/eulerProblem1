cfbab6f42644cdafe375d9410c39abd71670d255
Lorrie Pearson <pearsonlorrie@gmail.com>
2017-04-21 22:43:58 -0400

    added problem details and solution

---
+++ euproblem1.js
@@ -0,0 +1,10 @@
+// create a variable to hold the sum
+var num = 0;
+//For loop to iterate to 1000
+for (var i = 0; i < 1000; i++) {
+//multiples of 3 and 5
+if (i % 3 === 0 || i % 5 === 0) {
+//add to num
+num += i;
+}
+}
